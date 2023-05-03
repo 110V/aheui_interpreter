40f01ed8adafbcc836c806546827a662a39d91bd
110V <sgb1616@gmail.com>
2023-05-03 17:42:02 +0900

    feat : change command structure

--- src/command.rs
+++ src/command.rs
@@ -1,19 +1,32 @@
-pub enum OutType{
+#[derive(PartialEq,Debug)]
+pub enum IOType{
     Char,
     Number,
-    None,
 }
+
+#[derive(PartialEq,Debug)]
+pub enum PushType{
+    Input(IOType),
+    Value(i32),
+}
+#[derive(PartialEq,Debug)]
+pub enum PopType{
+    Output(IOType),
+    Discard,
+}
+
+#[derive(PartialEq,Debug)]
 pub enum Command{
     Add,
     Mul,
     Sub,
     Div,
     Rem,
-    Pop(OutType),
-    Push(i32),
+    Pop(PopType),
+    Push(PushType),
     Dup,
     Swap,
-    Set(i32),
+    Set(usize),
     Move,
     Comp,
     If,
