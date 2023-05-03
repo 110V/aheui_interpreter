b01caf7e7e327d339c6ad301fb5e50033554faa1
110V <sgb1616@gmail.com>
2023-05-04 02:46:58 +0900

    fix : fix bugs and add test

--- src/instruction_map.rs
+++ src/instruction_map.rs
@@ -8,24 +8,28 @@ pub struct InstructionMap{
 impl InstructionMap{
     pub fn from_str(map:&str) -> Self{
         let parser = InstructionParser::new();
-        let data: Vec<Vec<Tile>> = Vec::new();
+        let mut data: Vec<Vec<Tile>> = Vec::new();
 
         let mut width = 0;
-        let height = data.len();
+
 
         for line in map.lines(){
             let mut line_vec: Vec<Tile> = Vec::new();
-            if line.len() > width{
-                width = line.len();
+            let chars = line.chars();
+            let &count = &chars.count();
+            if  count > width{
+                width = count;
             }
             for instruction in line.chars(){
                 if parser.check_instruction_range(instruction){
                     line_vec.push(Some(instruction));
+                    continue;
                 }
                 line_vec.push(None);
             }
+            data.push(line_vec);
         }
-        
+        let height = data.len();
         Self{
             size:(width,height),
             data,
@@ -47,4 +51,19 @@ impl InstructionMap{
         self.size.1
     }
 
+}
+
+#[test]
+fn test_from_str() {
+    let map_str = "가나다\n라마";
+    let map = InstructionMap::from_str(map_str);
+    assert_eq!(map.get_width(), 3);
+    assert_eq!(map.get_height(), 2);
+    assert_eq!(map.get(0, 0), Ok(Some('가')));
+    assert_eq!(map.get(1, 0), Ok(Some('나')));
+    assert_eq!(map.get(2, 0), Ok(Some('다')));
+    assert_eq!(map.get(0, 1), Ok(Some('라')));
+    assert_eq!(map.get(1, 1), Ok(Some('마')));
+    assert_eq!(map.get(2, 1), Ok(None));
+    assert_eq!(map.get(3, 0), Err(String::from("Instruction Map Out of range: (3,0)")));
 }
