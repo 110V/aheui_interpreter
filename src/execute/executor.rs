4d32bfa379435005ee14ef59df92a5922c9c02fe
110V <sgb1616@gmail.com>
2023-05-04 12:43:09 +0900

    feat : implement all command execution

--- src/execute/executor.rs
+++ src/execute/executor.rs
@@ -3,24 +3,26 @@ use crate::instruction::Instruction;
 use crate::instruction_map::InstructionMap;
 use crate::instruction_parser::InstructionParser;
 use crate::memory::Memory;
-use crate::command::{Command, OutType};
+use crate::command::{Command, IOType, PushType, PopType};
 use crate::memory::{Queue,Stack};
 
 use super::command_result::CommandResult;
 
 
-struct executor{
+pub struct Executor{
     current_memory: usize,
     memories: Vec<Box<dyn Memory>>,
     output: String,
+    input: Vec<char>,
     map: InstructionMap,
     dir:Direction,
     cursor:(usize,usize),
     parser: InstructionParser,
+    quit:bool,
 }
 
-impl executor{
-    pub fn new(map:&str) -> executor{
+impl Executor{
+    pub fn new(map:&str,input:&str) -> Executor{
         let mut memories: Vec<Box<dyn Memory>> = Vec::new();
         for _ in 0..26{
             memories.push(Box::new(Stack::new()));
@@ -32,23 +34,80 @@ impl executor{
         let dir = Direction::Down;
         let cursor = (0,0);
         let parser = InstructionParser::new();
-        executor{
+        let input = input.chars().rev().collect();
+        Executor{
             current_memory,
             memories,output:"".to_string(),
+            input,
             map:InstructionMap::from_str(map),
             dir,
             cursor,
             parser,
+            quit:false
         }
     }
+    pub fn get_output(&self) -> &str{
+        &self.output
+    }
+    pub fn excute(&mut self){
+        while !self.quit{
+            self.run_current_instruction();
+        }
+    }
+
+    pub fn run_movement(&mut self, movement: Movement){
+        match movement{
+            Movement::Move(direction,distance) => {
+                self.dir = direction;
+                for _ in 0..distance{
+                    self.move_cursor();
+                }
+            },
+            Movement::Bounce =>  {
+                self.flip_current_dir();
+                self.move_cursor();
+            },
+            Movement::VertLine =>  {
+                match &self.dir{
+                    Direction::Left => self.dir = Direction::Right,
+                    Direction::Right => self.dir = Direction::Left,
+                    _ => {}
+                }
+                self.move_cursor();
+            },
+            Movement::HorLine =>  {
+                match &self.dir{
+                    Direction::Up => self.dir = Direction::Down,
+                    Direction::Down => self.dir = Direction::Up,
+                    _ => {}
+                }
+                self.move_cursor();
+            },
+            Movement::Slip => {
+                self.move_cursor();
+            }
+        }
+  
+    }
+
+
     pub fn move_cursor(&mut self){
         let (x,y) = self.cursor;
 
         let mut _x = x as i32;
         let mut _y = y as i32;
 
-        _x = _x % self.map.get_width() as i32;
-        _y = _y % self.map.get_height() as i32;
+        match &self.dir{
+            Direction::Up => _y -= 1,
+            Direction::Down => _y += 1,
+            Direction::Left => _x -= 1,
+            Direction::Right => _x += 1,
+            _ => unreachable!(),
+        }
+        let w = self.map.get_width() as i32;
+        let h = self.map.get_height() as i32;
+        _x = (w +_x) % w;
+        _y = (h+ _y) % h;
 
         self.cursor = (_x as usize,_y as usize);
     }
@@ -67,6 +126,7 @@ impl executor{
     pub fn out_char(&mut self, data: i32)->CommandResult{
         let c = std::char::from_u32(data as u32).ok_or("Invalid char")?;
         self.output.push(c);
+        //println!("{}",c);
         Ok(())
     }
 
@@ -77,104 +137,169 @@ impl executor{
         self.dir = dir;
     }
 
-    pub fn flip_dir(&mut self){
-        let dir = &self.dir;
-        let new_dir = match dir{
-            Direction::Up => Direction::Down,
-            Direction::Down => Direction::Up,
-            Direction::Left => Direction::Right,
-            Direction::Right => Direction::Left,
-            Direction::None => Direction::None,
-        };
-        self.dir = new_dir;
+    pub fn flip_current_dir(&mut self){
+        self.dir = self.dir.flip();
     }
 
     pub fn run_current_instruction(&mut self){
-        // let instruction = self.map.get(self.cursor.0,self.cursor.1)?;
-        // if let Some(instruction) = instruction{
-        //     let instruction = self.parser.parse_instruction(instruction)?;
-        //     self.run_instruction(instruction)?;
-        // }
+        let instruction = self.map.get(self.cursor.0,self.cursor.1).unwrap_or_else(|e|panic!("{}",e));
+
+        if let Some(instruction) = instruction{
+            println!("{}",instruction);
+            let instruction = self.parser.parse_instruction(instruction);
+            if self.quit{
+                return;
+            }
+            self.run_instruction(instruction);
+
+        }
+        else{
+            self.move_cursor();
+        }
     }
 
-    pub fn run_instruction(&mut self,instruction:Instruction)->CommandResult{
+
+    pub fn run_instruction(&mut self,instruction:Instruction){
         let command = instruction.command;
-        let movement = instruction.movement;
-        let mut distance = 1;
-
-        if let Movement::Move(dir,d) = movement{
-            self.set_dir(dir);
-            distance = d;
-        }
-
+        let mut movement = instruction.movement;
+        
         if let Err(_) = self.run_command(command){
-            self.flip_dir();
+            if let Movement::Move(dir,d) = movement{
+                movement = Movement::Move(dir.flip(),d);
+            }
+            else{
+                self.flip_current_dir();
+            }
         }
+        self.get_current_memory().print();
+        self.run_movement(movement)
+    }
 
-        self.move_cursor();
+    pub fn push_current_memory(&mut self, data:i32){
+        let current_memory = self.get_current_memory();
+        current_memory.push(data);
+    }
+
+    pub fn get_input(&mut self,io_type:IOType)->CommandResult{
+        let c:char = self.input.pop().ok_or("no input")?;
+
+        match io_type{
+            IOType::Number => {
+                let num = c.to_digit(10).ok_or("not a number")?;
+                self.push_current_memory(num as i32);
+            },
+            IOType::Char => {
+                let num = c as i32;
+                self.push_current_memory(num);
+            },
+        }
         Ok(())
+    }
+
+    pub fn check_elements_exist(&mut self,count:usize)->CommandResult{
+        let current_memory = self.get_current_memory();
+        if current_memory.len() < count{
+            return Err("not enough element".to_string());
+        }
+        Ok(())
+    }
+    pub fn pop_one(&mut self)->Result<i32,String>{
+        let current_memory = self.get_current_memory();
+        let value = current_memory.pop().ok_or("no element")?;
+        Ok(value)
+    }
+
+    pub fn pop_pair(&mut self)->Result<(i32,i32),String>{
+        self.check_elements_exist(2)?;
+        let current_memory = self.get_current_memory();
+        let a = current_memory.pop().unwrap();
+        let b = current_memory.pop().unwrap();
+        Ok((a,b))
     }
 
     pub fn run_command(&mut self, command:Command)->CommandResult{
         match command{
-            Command::Push(data) => {
-                let current_memory = self.get_current_memory();
-                current_memory.push(data);
-            },
-            Command::Pop(out_type) => {
-                let current_memory = self.get_current_memory();
-                let value = current_memory.pop();
-                match out_type{
-                    OutType::Number => {
-                        if let Some(value) = value{
-                            self.out_number(value);
-                        }
+            Command::Push(push_type) => {
+                match push_type {
+                    PushType::Input(io_type) => {
+                        self.get_input(io_type)?;
                     },
-                    OutType::Char => {
-                        if let Some(value) = value{
-                            self.out_char(value)?;
-                        }
+                    PushType::Value(value) => {
+                        self.push_current_memory(value);
                     },
-                    OutType::None => {
-                        
-                    }
                 }
             },
+            Command::Pop(pop_type) => {
+                let current_memory = self.get_current_memory();
+                let value = current_memory.pop().ok_or("no element")?;
+                if let PopType::Output(io_type) = pop_type{
+                    match io_type{
+                        IOType::Number => {
+                            self.out_number(value);
+                            
+                        },
+                        IOType::Char => {
+                            self.out_char(value)?;
+                        },
+                    }
+                }
+
+            },
             Command::Add => {
-                
+                let (a,b) = self.pop_pair()?;
+                self.push_current_memory(a+b);
             },
             Command::Sub => {
-
+                let (a,b) = self.pop_pair()?;
+                self.push_current_memory(b-a)
             },
             Command::Mul => {
-
+                let (a,b) = self.pop_pair()?;
+                self.push_current_memory(a*b)
             },
             Command::Div => {
-
+                let (a,b) = self.pop_pair()?;
+                self.push_current_memory(b/a)
             },
             Command::Rem => {
-
+                let (a,b) = self.pop_pair()?;
+                self.push_current_memory(b%a)
             },
             Command::Dup => {
-
+                let current_memory = self.get_current_memory();
+                let value = current_memory.pop().ok_or("no element")?;
+                self.push_current_memory(value);
+                self.push_current_memory(value);
             },
             Command::Swap => {
-
+                let (a,b) = self.pop_pair()?;
+                self.push_current_memory(a);
+                self.push_current_memory(b);
             },
             Command::Set(data) => {
-
+                self.current_memory = data;
             },
-            Command::Move => {
-
+            Command::Move(index) => {
+                let data = self.pop_one()?;
+                self.memories[index].push(data);
             },
             Command::Comp => {
-
+                let (a,b) = self.pop_pair()?;
+                if a > b{
+                    self.push_current_memory(0);
+                }
+                else{
+                    self.push_current_memory(1);
+                }
             },
             Command::If => {
-
+                let data = self.pop_one()?;
+                if data == 0{
+                    self.flip_current_dir();
+                }
             },
             Command::Quit => {
-
+                self.quit = true;
             },
             Command::None => {
 
