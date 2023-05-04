2ec322cdbbdf30566408b08f0b2b0f6d28b51739
110V <sgb1616@gmail.com>
2023-05-04 23:44:58 +0900

    fix : remove read char trim

--- src/stdin.rs
+++ src/stdin.rs
@@ -12,6 +12,7 @@ impl Stdin{
     }
     pub fn read_number(&mut self) -> i32{
         let mut buffer = string::String::new();
+        self. input = self.input.trim_start().to_string();
         while self.input.len() != 0{
             if let Some(c) = self.input.chars().nth(0){
                 if c.is_digit(10)||c=='-'{
@@ -34,8 +35,24 @@ impl Stdin{
         if self.input.len() == 0{
             return -1;
         }
-
         let c = self.input.remove(0);
         c as i32
     }
+}
+
+
+#[test]
+fn test_read_number(){
+    let mut stdin = Stdin::new("123");
+    assert_eq!(stdin.read_number(),123);
+}
+
+#[test]
+fn test_read_char(){
+    let mut stdin = Stdin::new("abc");
+    assert_eq!(stdin.read_char(),'a' as i32);
+    assert_eq!(stdin.read_char(),'b' as i32);
+    assert_eq!(stdin.read_char(),'c' as i32);
+    assert_eq!(stdin.read_char(),-1);
+    assert_eq!(stdin.read_char(),-1);
 }
